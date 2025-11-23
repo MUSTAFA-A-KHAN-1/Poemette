abbf6fc20b127707634b93dec7b73f14350bdfb5
MUSTAFA-A-KHAN-1 <mkhan62610@gmail.com>
2025-11-23 06:59:29 +0000

    Updates

--- _next/static/EcGmtmlZiV4Kji81ZufI3/_buildManifest.js
+++ _next/static/EcGmtmlZiV4Kji81ZufI3/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Poemette//_next/:path+",
-        "destination": "/Poemette/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
